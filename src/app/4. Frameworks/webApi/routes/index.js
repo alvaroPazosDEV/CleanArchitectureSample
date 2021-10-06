2cf3f095f0cc76be99ccf41c88c005fd737d053a
Alvaro Pazos <alvaropazos1997@gmail.com>
2021-10-05 23:19:12 -0500

    Added health route

--- src/app/4. Frameworks/webApi/routes/index.js
+++ src/app/4. Frameworks/webApi/routes/index.js
@@ -4,7 +4,10 @@ const order = require('./order');
 const apiRouter = (dependencies) => {
     const routes = express.Router()
     const orderRouter = order(dependencies)
+
     routes.use('/order', orderRouter)
+    routes.get('/health', (req, res) => res.send('OK'))
+
     return routes
 
 }
